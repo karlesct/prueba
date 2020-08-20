3ebd8609f1ac0baa7d3444b75f6541929a9ad27b
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 10:08:11 +0200

    [ADD] - Initial commit CoreAssembly

---
+++ prueba/pruebaCore/CoreAssembly.swift
@@ -0,0 +1,13 @@
+//
+//  CoreAssembly.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import Foundation
+
+public final class CoreAssembly {
+    
+}
