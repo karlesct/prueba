475ef723e836a5e5324d004b9ee81b7dc9483745
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 10:11:47 +0200

    [ADD] - Initial commit AppAssembly

---
+++ prueba/prueba/AppAssembly.swift
@@ -0,0 +1,16 @@
+//
+//  AppAssembly.swift
+//  prueba
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import Foundation
+
+internal final class AppAssembly {
+    
+    // MARK: - Properties
+    
+    
+}
