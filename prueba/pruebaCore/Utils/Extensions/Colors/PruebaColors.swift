d8615cf9fcb5030ad7ec5bc7246fecea2334ff2c
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 11:07:21 +0200

    [ADD] - UIColor+Hex (hexToColor) and PruebaColors (return color)

---
+++ prueba/pruebaCore/Utils/Extensions/Colors/PruebaColors.swift
@@ -0,0 +1,62 @@
+//
+//  PruebaColors.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import UIKit
+
+public final class PruebaColors {
+        
+    struct Primary {
+        
+        static let blueOne = UIColor(rgb: 0x003399)
+        static let blueTwo = UIColor(rgb: 0x000066)
+        static let white = UIColor(rgb: 0xFFFFFF)
+        static let blueOneSeventyFiveOfInk = UIColor(rgb: 0x4066B3)
+        
+    }
+    
+    struct Secondary {
+        
+        static let blackStone = UIColor(rgb: 0x6F6863)
+        static let darkStone = UIColor(rgb: 0xEEECEA)
+        static let gray = UIColor(rgb: 0xDDDDDD)
+        static let darkGray = UIColor(rgb: 0x25282D)
+        
+    }
+    
+    struct Complementary {
+        
+        static let teal = UIColor(rgb: 0x017692)
+        static let violet = UIColor(rgb: 0xA490B6)
+        static let violetTwo = UIColor(rgb: 0xA8BCD4)
+        static let salmonTwo = UIColor(rgb: 0xEA635C)
+        
+        static let green = UIColor(rgb: 0x28B47D)
+        static let greenTwo = UIColor(rgb: 0xC5E3A2)
+        static let lemon = UIColor(rgb: 0xE7E985)
+        static let turquoise = UIColor(rgb: 0x00BFB3)
+        
+        static let red = UIColor(rgb: 0xD34C47)
+        static let turquoiseTwo = UIColor(rgb: 0x9FD0C0)
+        static let orangeTwo = UIColor(rgb: 0xFFAF67)
+        static let rose = UIColor(rgb: 0xE0A6B4)
+        
+        static let blueSelected = UIColor(rgb: 0x009EE0)
+        static let blueDefault = UIColor(rgb: 0xB3C2E1)
+        static let greyDisabledBorder = UIColor(rgb: 0xd3CfCA)
+    }
+    
+    struct Semantic {
+        
+        static let blueInformative = UIColor(rgb: 0x4F90C8)
+        static let greenPositive = UIColor(rgb: 0x11A068)
+        static let redError = UIColor(rgb: 0xE32A23)
+        static let orangeWarning = UIColor(rgb: 0xD8620C)
+        
+    }
+
+}
---
+++ prueba/pruebaCore/Utils/Extensions/Colors/UIColor+Hex.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Hex.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        self.init(red: CGFloat(red) / 255.0,
+                  green: CGFloat(green) / 255.0,
+                  blue: CGFloat(blue) / 255.0,
+                  alpha: alpha)
+    }
+    
+    convenience init(rgb: Int, alpha: CGFloat = 1.0) {
+        self.init(red: (rgb >> 16) & 0xFF,
+                  green: (rgb >> 8) & 0xFF,
+                  blue: rgb & 0xFF,
+                  alpha: alpha)
+    }
+}
