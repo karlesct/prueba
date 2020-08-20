74546da88c266d93741500f78f1df23337f595e9
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 20:26:59 +0200

    [UPDATE] - AppAssembly to use CoreAssembly

--- prueba/prueba/Assembly/AppAssembly.swift
+++ prueba/prueba/Assembly/AppAssembly.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import pruebaCore
 
 internal final class AppAssembly {
     
     // MARK: - Properties
     
     private(set) lazy var window = UIWindow(frame: UIScreen.main.bounds)
+    private(set) lazy var navigationController: UINavigationController = {
+        return PruebaNavigationController()
+    }()
+    private(set) lazy var coreAssembly = CoreAssembly(navigationController: navigationController)
+    
     
 }
