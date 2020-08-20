07311315561f8d82400f0d337ff6263204b0b48e
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 10:56:13 +0200

    [ADD] - String+Empty(returns "") & String+Localized (translation)

---
+++ prueba/pruebaCore/Utils/Extensions/Strings/String+Empty.swift
@@ -0,0 +1,13 @@
+//
+//  String+Empty.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+public extension String {
+    
+    static let empty = ""
+    
+}
---
+++ prueba/pruebaCore/Utils/Extensions/Strings/String+Localized.swift
@@ -0,0 +1,19 @@
+//
+//  String+Localized.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+public extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self,
+                                 tableName: "Localizable",
+                                 bundle: Bundle.CoreIdentifier ?? .main,
+                                 value: "**\(self)**",
+            comment: "")
+    }
+    
+}
