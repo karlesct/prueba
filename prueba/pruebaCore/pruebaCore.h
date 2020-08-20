d623c6a484757116c236b253ba24c9d233e668b5
ccanadas <carles.canadas.torrents@everis.com>
2020-08-20 09:57:00 +0200

    [ADD] - Initial commit framework for features

---
+++ prueba/pruebaCore/pruebaCore.h
@@ -0,0 +1,19 @@
+//
+//  pruebaCore.h
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pruebaCore.
+FOUNDATION_EXPORT double pruebaCoreVersionNumber;
+
+//! Project version string for pruebaCore.
+FOUNDATION_EXPORT const unsigned char pruebaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pruebaCore/PublicHeader.h>
+
+
