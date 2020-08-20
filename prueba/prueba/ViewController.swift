97cafe62c5b561226cbe3dfae9d2f0d4fb8f6829
karlesct <54058145+karlesct@users.noreply.github.com>
2020-08-20 09:47:50 +0200

    Add files via upload
    Initial commit

---
+++ prueba/prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Carles Cañadas Torrents on 20/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
