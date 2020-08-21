6261ee5086cc1b4faced6d2c8cbdf339e0ee744a
ccanadas <carles.canadas.torrents@everis.com>
2020-08-21 16:37:41 +0200

    [ADD] - NibLoadableTest + ResusableCellTest

---
+++ prueba/pruebaCoreTests/Utils/Cell/NibLoadableTest.swift
@@ -0,0 +1,25 @@
+//
+//  NibLoadableTest.swift
+//  pruebaCoreTests
+//
+//  Created by Carles Cañadas Torrents on 21/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import XCTest
+
+class NibLoadableTest: XCTestCase {
+    
+    func testNibLoadable_01() {
+        
+        let testCellNibName = TestCell.nibName
+        XCTAssertEqual(testCellNibName, "TestCell")
+    }
+    
+    func testNibLoadable_02() {
+        
+        let testCell = TestCell.instantiate()
+        XCTAssertNotNil(testCell)
+    }
+    
+}
---
+++ prueba/pruebaCoreTests/Utils/Cell/ReusableCellTest.swift
@@ -0,0 +1,19 @@
+//
+//  ReusableCellTest.swift
+//  pruebaCoreTests
+//
+//  Created by Carles Cañadas Torrents on 21/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import XCTest
+
+class ReusableCellTest: XCTestCase {
+    
+    func testReusableCell_01() {
+        
+        let reuseIdentifier = TestCell.reuseIdentifier
+        XCTAssertEqual(reuseIdentifier, "TestCell")
+    }
+    
+}
