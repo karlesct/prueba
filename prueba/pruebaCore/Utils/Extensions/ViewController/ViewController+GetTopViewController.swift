183207a09a086007d737557a6f0a1a3d1a157da2
ccanadas <carles.canadas.torrents@everis.com>
2020-08-21 09:26:55 +0200

    [ADD] - ViewController+GetTopViewController & Window+VisibleViewController that methods are used to retrieve te top view controller of the view hierarchy

---
+++ prueba/pruebaCore/Utils/Extensions/ViewController/ViewController+GetTopViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController+GetTopViewController.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 21/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    var getTopViewController: UIViewController? {
+        let appDelegate = UIApplication.shared.delegate
+        if let window = appDelegate?.window {
+            return window?.visibleViewController
+        }
+        return nil
+    }
+    
+    
+}
---
+++ prueba/pruebaCore/Utils/Extensions/Window/Window+VisibleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Window+VisibleViewController.swift
+//  pruebaCore
+//
+//  Created by Carles Cañadas Torrents on 21/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import UIKit
+
+public extension UIWindow {
+    var visibleViewController: UIViewController? {
+        return UIWindow.getVisibleViewControllerFrom(vc: self.rootViewController)
+    }
+
+    static func getVisibleViewControllerFrom(vc: UIViewController?) -> UIViewController? {
+        if let nc = vc as? UINavigationController {
+            return UIWindow.getVisibleViewControllerFrom(vc: nc.visibleViewController)
+        } else if let tc = vc as? UITabBarController {
+            return UIWindow.getVisibleViewControllerFrom(vc: tc.selectedViewController)
+        } else {
+            if let pvc = vc?.presentedViewController {
+                return UIWindow.getVisibleViewControllerFrom(vc: pvc)
+            } else {
+                return vc
+            }
+        }
+    }
+}
